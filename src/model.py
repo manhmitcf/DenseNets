ea41a5589046db59362348a5fb91deca5ecc83dc
manhmitcf <manhmit1221@gmail.com>
2025-04-09 00:54:38 +0700

    first commit

---
+++ src/model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from torchvision.models import DenseNet, DenseNet121_Weights
+
+class FishClassifier(nn.Module):
+    def __init__(self, num_classes=3): 
+        super(FishClassifier, self).__init__()
+        self.desnet = DenseNet(weights=DenseNet121_Weights.DEFAULT)
+
+        self.desnet.classifier = nn.Sequential(
+            nn.Linear(self.desnet.classifier.in_features, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes)
+        )
+
+
+    def forward(self, x):
+        return self.desnet(x)
